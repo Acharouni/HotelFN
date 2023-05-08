dc1e78d1b98b462e90448228f02c4e58eccd276e
Acharouni <aymencharouni@outlook.fr>
2023-05-08 23:12:24 +0100

    update !!

--- app/src/main/java/com/example/hotel/ui/about/AboutViewModel.java
+++ app/src/main/java/com/example/hotel/ui/about/AboutViewModel.java
@@ -6,14 +6,5 @@ import androidx.lifecycle.ViewModel;
 
 public class AboutViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
 
-    public AboutViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
--- app/src/main/java/com/example/hotel/ui/about/about.java
+++ app/src/main/java/com/example/hotel/ui/about/about.java
@@ -1,34 +1,122 @@
 package com.example.hotel.ui.about;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
+import com.bumptech.glide.Glide;
 import com.example.hotel.R;
+import com.example.hotel.rooM1;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class about extends Fragment {
+    private RecyclerView mRecyclerView;
+    private MyAdapter mAdapter;
 
-
-
-    private AboutViewModel mViewModel;
-
-    public static com.example.hotel.ui.about.about newInstance() {
-        return new com.example.hotel.ui.about.about();
-    }
+    private StorageReference mStorageRef;
+    private List<String> mImageUrls;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View V = inflater.inflate(R.layout.fragment_about, container, false);
+        mRecyclerView = V.findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(),
+                LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference().child("upload");
+
+        mImageUrls = new ArrayList<>();
+        mAdapter = new MyAdapter(mImageUrls);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mStorageRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+            @Override
+            public void onSuccess(ListResult listResult) {
+                for (StorageReference item : listResult.getItems()) {
+                    item.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            mImageUrls.add(uri.toString());
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        });
+
+        return V;
+
     }
 
+    private static class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+        private List<String> mImageUrls;
+
+        public MyAdapter(List<String> imageUrls) {
+            mImageUrls = imageUrls;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            String imageUrl = mImageUrls.get(position);
+
+            // Charger l'image à partir de l'URL de téléchargement avec Glide
+            Glide.with(holder.itemView.getContext())
+                    .load(imageUrl)
+                    .into(holder.mImageView);
+
+            holder.mImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, rooM1.class);
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mImageUrls.size();
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public ImageView mImageView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                mImageView = itemView.findViewById(R.id.imageView);
+                mImageView.setAdjustViewBounds(true);
+            }
+        }
+    }
 }
